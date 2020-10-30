97d627aa94132c3b3a64140edb6a221bd4595bab
sasha <drjohnes1973@gmail.com>
2020-10-30 18:03:05 +0600

    Commit files to pull

--- augbuilder/layout.py
+++ augbuilder/layout.py
@@ -1,44 +1,25 @@
 def return_layout():
     """
     Returns layout.
-
     Returns:
         9 elements for transformation example 
     """
     return """
     <style>
-
-    .main{
-        overflow-x: hidden !important;
-    }
     .main .block-container > div{
-
         width: 130% !important;
         display: flex;
         flex-direction: row;
         flex-wrap: wrap;
-        gap: 2.5rem;
     }
     
-    .main .element-container:nth-child(6),
     .main .element-container:nth-child(4),
     .main .element-container:nth-child(5){
-        width: 100% !important;
-
-    }
-
-    .main .element-container:nth-child(5){
-    height: 0% !important;
-
-    }
-
-    .main .element-container:nth-child(1),
-    .main .element-container:nth-child(2){
-        width: 45% !important;
+        width: 0% !important;
     } 
    
-    .main .element-container:nth-child(15),
-    .main .element-container:nth-child(16),
+    .main .element-container:nth-child(6),
+    .main .element-container:nth-child(7),
     .main .element-container:nth-child(8),
     .main .element-container:nth-child(9),
     .main .element-container:nth-child(10),
@@ -46,22 +27,16 @@ def return_layout():
     .main .element-container:nth-child(12),
     .main .element-container:nth-child(13),
      .main .element-container:nth-child(14){
-        width: 30% !important;
-        height: 30% !important;
+        width: 33% !important;
+        height: 33% !important;
     }
-
+    
     .main .stFileUploader > div,
     .main .stFileUploader > label{
         width: 98% !important;
     }
-    .main .stImage {
-        width: 40% !important;
-        
-    }
-
     .main .stImage > img{
-        width: 100% !important;
-        
+        width: 40% !important;
     }
     </style>
     """
